6567ba8fe6271b9d0bd8a16beaa52163c9b7a61e
Jimvid <victor@jimvid>
2021-10-11 20:23:15 +0200

    Remove links from footer

--- components/organisms/footer/Footer.tsx
+++ components/organisms/footer/Footer.tsx
@@ -8,13 +8,8 @@ import S from "./footer.module.scss"
 
 const Footer = () => {
   const globalContext = useContext(GlobalContext) as Props
-  const { footer, menu } = globalContext
+  const { footer } = globalContext
   const { logotype, title, text } = footer
-
-  // Ordering the menu links
-  const menuLinks = menu.sort((a, b) => {
-    return parseFloat(a.menuOrder) - parseFloat(b.menuOrder)
-  })
 
   return (
     <footer className={S.footer}>
@@ -29,13 +24,6 @@ const Footer = () => {
           {title}
         </Title>
         <p className={`body-small ${S.text}`}>{text}</p>
-        <ul className={S.list}>
-          {menuLinks.map((m: MenuItem) => (
-            <li key={m.slug} className={S.list__item}>
-              <Link href={m.slug}>{m.title}</Link>
-            </li>
-          ))}
-        </ul>
       </div>
     </footer>
   )
@@ -50,13 +38,6 @@ interface Props {
       url: string
     }
   }
-  menu: MenuItem[]
-}
-
-interface MenuItem {
-  slug: string
-  title: string
-  menuOrder: string
 }
 
 export default Footer
