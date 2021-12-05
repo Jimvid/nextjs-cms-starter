9bed2f3e29bf159d8af32833676353360015c6c1
Jimvid <victor@jimvid>
2021-12-05 20:18:13 +0100

    Title: Generate IDs based on title

--- components/atoms/title/index.tsx
+++ components/atoms/title/index.tsx
@@ -1,9 +1,35 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 export const Title = ({ children, size, className }: Props) => {
   if (!children) return null
+  const [generatedID, setGeneratedId] = useState("")
 
-  const titleElement = React.createElement(size, { className }, children)
+  useEffect(() => {
+    const EXCLUDED_CHARS = ["!", "?", "å", "ä", "ö", " "]
+    const REPLACED_CHARS = ["", "", "a", "a", "o", "-"]
+
+    // Split title into array
+    const titleArr = Array.from(children.toLowerCase())
+
+    // Loop through the string and replace EXCLUDED with REPLACED
+    for (let i = 0; i < EXCLUDED_CHARS.length; i++) {
+      for (let j = 0; j < titleArr.length; j++) {
+        if (titleArr[j] === EXCLUDED_CHARS[i]) {
+          titleArr[j] = REPLACED_CHARS[i]
+        }
+      }
+    }
+
+    // Join array and set ID in state
+    const id = titleArr.join("")
+    setGeneratedId(id)
+  }, [])
+
+  const titleElement = React.createElement(
+    size,
+    { className, id: generatedID },
+    children
+  )
 
   return titleElement
 }
